d456b0637383b2a9ddeb293611b38c16e7e386c7
Nic0 <nicolas.caen@gmail.com>
2010-10-11 16:04:29 +0200

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -10,12 +10,13 @@ end
 group :test do
   gem 'rspec', '2.0.0.rc'
   gem 'webrat', '0.7.1'
+  gem 'spork', '0.8.4'
 end
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-#gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'sqlite3-ruby', :require => 'sqlite3'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+        :content => "My awesome site | Home")
     end
   end
 
@@ -13,7 +19,24 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+        :content => "My awesome site | Contact")
     end
+
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+        :content => "My awesome site | About")
+    end
+  end
 end
